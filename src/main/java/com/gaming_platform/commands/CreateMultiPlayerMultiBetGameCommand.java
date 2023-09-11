ec75ab29fc5c1105ee86fe2529a9e41f30a13be4
David Miguel Gaite Cruz <david@onoffapp.com>
2023-09-11 15:54:49 +0300

    Comments

--- src/main/java/com/gaming_platform/commands/CreateMultiPlayerMultiBetGameCommand.java
+++ src/main/java/com/gaming_platform/commands/CreateMultiPlayerMultiBetGameCommand.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class CreateMultiPlayerMultiBetGameCommand {
+    // Consider adding validation logic for your command objects.Check Spring provides annotations like @Valid and validation constraints like @NotNull, @Size, etc.
     private String gameName;
     private List<CreatePlayerCommand> createPlayerCommands;
 }
